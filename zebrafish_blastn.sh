e41afbec00f41b89a6c331c747466d7d985e3984
Katherine Eaton <66837313+kmeaton@users.noreply.github.com>
2021-12-13 16:28:07 -0600

    Create zebrafish_blastn.sh

---
+++ zebrafish_blastn.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# KM Eaton, Auburn University, 2021
+# Code associated with Eaton et al. 2021 Frontiers in Ecology and Evolution
+# This code was run on the Alabama Supercomputer. 
+
+# Source and load the required modules for this script
+source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+module load blast+/2.6.0
+
+# Create the blast database
+makeblastdb -in zebrafish_full_gene_14june21.fa -input_type fasta -dbtype nucl -out zebrafish_full_gene_14june21.fa.DB
+
+blastn -db zebrafish_full_gene_14june21.fa.DB -query tilapia_unannotated_contigs.fa -out zebrafish_full_gene.OUT -evalue 1e-10 -num_threads 16 -outfmt '6 std qcovhsp' -max_target_seqs 5 
