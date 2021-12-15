958d61bfd9e119ec7e14e2b25d73ec410e92df4c
Katherine Eaton <66837313+kmeaton@users.noreply.github.com>
2021-12-15 15:43:08 -0600

    Add files via upload

--- DESeq2_adults.R
+++ DESeq2_adults.R
@@ -37,24 +37,30 @@ livercoldata<-subset(adultallcoldata, tissue == "liver")
 livercountdata<-subset(adultallcountdata, select=c("C1.1L.trim.sam.counts.isoform","C1.5L.trim.sam.counts.isoform","C1.6L.trim.sam.counts.isoform","C2.2L.trim.sam.counts.isoform","C2.5L.trim.sam.counts.isoform","C2.7L.trim.sam.counts.isoform","H1.1L.trim.sam.counts.isoform","H1.2L.trim.sam.counts.isoform","H1.3L.trim.sam.counts.isoform","H2.1L.trim.sam.counts.isoform","H2.2L.trim.sam.counts.isoform","H2.3L.trim.sam.counts.isoform","M1.1L.trim.sam.counts.isoform","M1.3L.trim.sam.counts.isoform","M1.4L.trim.sam.counts.isoform","M2.2L.trim.sam.counts.isoform","M2.4L.trim.sam.counts.isoform"))
 all(rownames(livercoldata) == colnames(livercountdata))
 
-################ Exposure time - not necessary #########################
+# Run a likelihood ratio test to determine if the effect of exposure time is significant
+# First muscle
 Mdds<-DESeqDataSetFromMatrix(countData = musclecountdata, colData = musclecoldata, design =~ resp + exposure_bin + treatment)
 Mdds<-DESeq(Mdds)
 
 # This gives you the number of genes due to exposure time
-musLRT<-DESeq(Mdds, test="LRT", reduced=~resp + exposure_bin)
-musLRTres<-results(musLRT, alpha = 0.05)
-summary(musLRTres)
+musLRTexp<-DESeq(Mdds, test="LRT", reduced=~resp + treatment)
+musLRTexp.res<-results(musLRTexp, alpha = 0.05)
+summary(musLRTexp.res)
 # Zero genes significant due to exposure time, okay to exclude
 
+# Then liver
 Ldds<-DESeqDataSetFromMatrix(countData = livercountdata, colData = livercoldata, design =~ resp + exposure_bin + treatment)
 Ldds<-DESeq(Ldds)
-liverLRT<-DESeq(Ldds, test = "LRT", reduced =~ resp + exposure_bin)
-liverLRTres<-results(liverLRT, alpha = 0.05)
-summary(liverLRTres)
+
+# This gives you the number of genes significant due to exposure time
+liverLRTexp<-DESeq(Ldds, test = "LRT", reduced =~ resp + treatment)
+liverLRTexp.res<-results(liverLRTexp, alpha = 0.05)
+summary(liverLRTexp.res)
 # Two genes significant due to exposure time, okay to exclude
 
-#######################################################################
+# The code from above shows that the effect of exposure time is negligible and it is okay to exclude it from our further analyses
+
+# Now we can do pairwise comparisons for liver and muscle
 
 ## MUSCLE ##
 Mdds<-DESeqDataSetFromMatrix(countData = musclecountdata, colData = musclecoldata, design =~ resp + treatment)
@@ -101,16 +107,14 @@ summary(Mres_24_v_21)
 #low counts [2]     : 50258, 60%
 #(mean count < 0)
 
-# PCA
+# LRT and PCA
 musLRT<-DESeq(Mdds, test="LRT", reduced=~resp)
 musLRTres<-results(musLRT, alpha = 0.05)
 summary(musLRTres)
 musLRTvsd<-vst(musLRT)
-plotPCA(musLRTvsd, intgroup = "exposure_bin") # This is with all genes, PC1 is respirometry :/, PC2 is something that I don't know
-# We want just the significant genes
 musLRTsigs<-subset(musLRTres, padj < 0.05)
-sigs<-rownames(musLRTsigs)
-VSD.mus.subset <- musLRTvsd[rownames(musLRTvsd) %in% sigs, ]
+mus_sigs<-rownames(musLRTsigs)
+VSD.mus.subset <- musLRTvsd[rownames(musLRTvsd) %in% mus_sigs, ]
 summary(VSD.mus.subset)
 plotPCA(VSD.mus.subset, intgroup = "treatment")
 adult_muscle_PCA_dim<-plotPCA(VSD.mus.subset, intgroup = "treatment", returnData = T)
@@ -118,6 +122,7 @@ ggplot(adult_muscle_PCA_dim, aes(PC1, PC2)) + geom_point(color = as.factor(adult
   labs(x = "PC1: 31% variance", y = "PC2: 23% variance") + ggtitle("Adult muscle")
 ggsave("~/Box/Bernal_lab/Pinfish/ThermalStress_Project_PAPER/Figures/Figure4_adult_muscle.svg", width = 5, height = 3)
 
+# Now run the same for liver
 # LIVER
 Ldds<-DESeqDataSetFromMatrix(countData = livercountdata, colData = livercoldata, design =~ resp + treatment)
 Ldds$treatment<-relevel(Ldds$treatment, ref="21")
@@ -165,19 +170,33 @@ summary(Lres_24_v_21)
 #low counts [2]     : 19837, 17%
 #(mean count < 0)
 
-# Validation
+# LRT and PCA
 livLRT<-DESeq(Ldds, test="LRT", reduced=~resp)
 livLRTres<-results(livLRT, alpha = 0.05)
 summary(livLRTres)
 livLRTvsd<-vst(livLRT)
-plotPCA(livLRTvsd, intgroup = "treatment") # This is with all genes, again resp is PC1, I don't know what PC2 is
 # We want just the significant genes
 livLRTsigs<-subset(livLRTres, padj < 0.05)
-sigs<-rownames(livLRTsigs)
-VSD.liv.subset <- livLRTvsd[rownames(livLRTvsd) %in% sigs, ]
+liv_sigs<-rownames(livLRTsigs)
+VSD.liv.subset <- livLRTvsd[rownames(livLRTvsd) %in% liv_sigs, ]
 summary(VSD.liv.subset)
 plotPCA(VSD.liv.subset, intgroup = "treatment") # Here a bit of overlap between control and +3, but clear distinction between both of those and +6
 adult_liver_PCA_dim<-plotPCA(VSD.liv.subset, intgroup = "treatment", returnData = T)
 ggplot(adult_liver_PCA_dim, aes(PC1, PC2)) + geom_point(color = as.factor(adult_liver_PCA_dim$treatment)) +
   labs(x = "PC1: 38% variance", y = "PC2: 27% variance") + ggtitle("Adult liver")
 ggsave("~/Box/Bernal_lab/Pinfish/ThermalStress_Project_PAPER/Figures/Figure4_adult_liver.svg", width = 5, height = 3)
+
+# Write output files to be used in GO term tests
+M_27_v_21_df<-as.data.frame(Mres_27_v_21)
+write.table(M_27_v_21_df, "muscle_27_21_pairwise_adult.tsv", sep = "\t", quote=F)
+M_24_v_21_df<-as.data.frame(Mres_24_v_21)
+write.table(M_24_v_21_df, "muscle_24_21_pairwise_adult.tsv", sep = "\t", quote=F)
+M_27_v_24_df<-as.data.frame(Mres_27_v_24)
+write.table(M_27_v_24_df, "muscle_27_24_pairwise_adult.tsv", sep = "\t", quote=F)
+
+L_27_v_21_df<-as.data.frame(Lres_27_v_21)
+write.table(L_27_v_21_df, "liver_27_21_pairwise_adult.tsv", sep = "\t", quote=F)
+L_24_v_21_df<-as.data.frame(Lres_24_v_21)
+write.table(L_24_v_21_df, "liver_24_21_pairwise_adult.tsv", sep = "\t", quote=F)
+L_27_v_24_df<-as.data.frame(Lres_27_v_24)
+write.table(L_27_v_24_df, "liver_27_24_pairwise_adult.tsv", sep = "\t", quote=F)
