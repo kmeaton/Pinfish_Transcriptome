f8ce9542cc44649d6af1aa1723fcd2cd627ae2d6
Katherine Eaton <66837313+kmeaton@users.noreply.github.com>
2022-01-10 09:34:28 -0600

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # KM Eaton, Auburn University, 2021
-# Code associated with Eaton et al. 2021 Frontiers in Ecology and Evolution
+# Code associated with Eaton et al. 2022 Frontiers in Ecology and Evolution
 # This code was run on the Alabama Supercomputer.
 
 sed -r 's/^(>[A-Za-z_0-9]+)\s.+$/\1/g' trinity_assembly_cdhit.fa > trinity_assembly_cdhit_truncatednames.fa
