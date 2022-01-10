a3f6468ce692759930f92b8b2671379b9a94c0c9
Katherine Eaton <66837313+kmeaton@users.noreply.github.com>
2022-01-10 09:38:13 -0600

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # KM Eaton, Auburn University, 2021
-# Code associated with Eaton et al. 2021 Frontiers in Ecology and Evolution
+# Code associated with Eaton et al. 2022 Frontiers in Ecology and Evolution
 # This code was run on the Alabama Supercomputer. 
 
 # First, make sure you have concatenated your raw reads into two files - one of all the mate 1 reads, and one of all the mate 2 reads.
